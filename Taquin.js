ae1c943091ed288bd860ccb3ee42aa9b1c5f7ad9
Carmelina NAPOLITANO <Carmelina NAPOLITANO@Digital-Grenoble.local>
2018-06-13 13:14:20 +0200

    page du jeu

---
+++ Taquin.js
@@ -0,0 +1,19 @@
+var taquin = new Array();
+taquin [0] = new Array ("1", "2", "3", "4");
+taquin [1] = new Array ("5", "6", "7", "8");
+taquin [2] = new Array ("9", "10", "11", "12");
+taquin [3] = new Array ("13", "14", "15", " ");
+
+
+function draw() {
+	for (i = 0; i <= 3; i ++) {
+		for (j = 0; j <= 3; j ++) {
+			console.log( " " + i + " " + j + " " + (taquin[i][j]));
+			
+		}
+	}
+	
+}
+
+
+draw();
