2512600b12dc03f9d9e7a4acd921422c525589e7
Carmelina NAPOLITANO <Carmelina NAPOLITANO@Digital-Grenoble.local>
2018-06-14 15:27:40 +0200

    Ajout fonction permutable

--- Taquin.js
+++ Taquin.js
@@ -3,7 +3,8 @@ taquin [0] = new Array ("1", "2", "3", "4");
 taquin [1] = new Array ("5", "6", "7", "8");
 taquin [2] = new Array ("9", "10", "11", "12");
 taquin [3] = new Array ("13", "14", "15", " ");
-
+var iVide = ;
+var jVide = ;
 
 function draw() {
 	for (i = 0; i <= 3; i ++) {
@@ -12,7 +13,11 @@ function draw() {
 			$("#p" + i + j).html(taquin[i][j]);
 		}
 	}
-	
+	$("button").click(function(){
+		var i = parseInt($(this).attr("i"));
+		var j = parseInt($(this).attr("j"));
+		console.log(permutable(i,j));
+	});
 }
 
 function exist(i, j) {
@@ -25,7 +30,19 @@ function exist(i, j) {
 }
 
 function empty(i, j) {
-	if ((taquin [i][j]) === "") {
+	if ((taquin [i][j]) === " ") {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+function permutable(i, j) {
+	if ((exist(i, j + 1)) && (empty(i, j + 1)) 
+		|| ((exist(i, j - 1)) && (empty (i, j - 1))) 
+		|| ((exist(i - 1, j)) && (empty(i - 1, j))) 
+		|| ((exist(i + 1, j)) && (empty(i + 1, j)))) {
 		return true;
 	}
 	else {
